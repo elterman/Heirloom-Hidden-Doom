b32675b7f37113ba63912ffaad4220c002b12634
Arkady Elterman <elterman@gmail.com>
2024-11-11 18:01:12 -0800

    .

--- src/Player.jsx
+++ src/Player.jsx
@@ -5,14 +5,15 @@ import Flag from './Images/Flag.webp';
 import { a_over } from './atoms';
 import { LOST, WON } from './const';
 
-const Player = ({ style }) => {
+const Player = () => {
     const [over] = useAtom(a_over);
 
     const gridArea = '1/2';
     const src = over === WON ? Coin : over === LOST ? Trap : Flag;
-    const scale = over === LOST ? 8/9 : 1;
+    const scale = over === LOST ? 8 / 9 : 1;
+    const style = over === LOST ? { transform: `scale(${scale})`, filter: 'sepia(0.7)' } : {};
 
-    return <img src={src} alt='avatar' width={90} style={{ gridArea, zIndex: 1, transform: `scale(${scale})` }} />;
+    return <img src={src} alt='avatar' width={90} style={{ gridArea, zIndex: 1, ...style }} />;
 };
 
 export default Player;
