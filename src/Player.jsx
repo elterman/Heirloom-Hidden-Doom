e9e29781d106789f2c01c053e0e4b15941ea8f7c
Arkady Elterman <elterman@gmail.com>
2024-11-11 17:49:09 -0800

    .

--- src/Player.jsx
+++ src/Player.jsx
@@ -1,22 +1,21 @@
-import Avatar from './Images/Avatar.webp';
+import { useAtom } from 'jotai';
 import Coin from './Images/Coin.webp';
-import Flag from './Images/Flag.webp';
 import Trap from './Images/Death.webp';
-import { _11 } from './const';
+import Flag from './Images/Flag.webp';
+import { a_over } from './atoms';
+import { _11, LOST, WON } from './const';
 
-const Player = ({ player, style }) => {
+const Player = ({ style }) => {
+    const [over] = useAtom(a_over);
+
     const gridArea = _11;
-    let src = Avatar;
-
-    if (player === 1) {
-        src = Coin;
-    }
+    const src = over === WON ? Coin : over === LOST ? Trap :Flag;
 
     const renderAvatar = () => {
         const width = 90;
 
         return <div className='avatar'>
-            <img src={Coin} alt='avatar' width={width} style={{ gridArea, zIndex: 1 }} />
+            <img src={src} alt='avatar' width={width} style={{ gridArea, zIndex: 1 }} />
         </div>;
     };
 
